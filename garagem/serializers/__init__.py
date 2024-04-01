640c7e1f7fac5d2d5a25d974a06b0a24630fd238
Santiago de Oliveira <santiagodeoliveira2004@gmail.com>
2024-04-01 09:43:52 -0300

    serializers veiculo

--- garagem/serializers/__init__.py
+++ garagem/serializers/__init__.py
@@ -3,4 +3,4 @@ from .acessorios import AcessoriosSerializers
 from .categoria import CategoriaSerializers
 from .cor import CorSerializers
 from .modelo import ModeloSerializer
-from .veiculo import VeiculoSerializer
+from .veiculo import VeiculoSerializer, VeiculoDetailSerializer, VeiculoListSerializer
--- garagem/views/veiculo.py
+++ garagem/views/veiculo.py
@@ -1,7 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
 from garagem.models import Veiculo
-from garagem.serializers import VeiculoSerializer
+from garagem.serializers import VeiculoSerializer, VeiculoListSerializer, VeiculoDetailSerializer
 
 class VeiculoViewSet(ModelViewSet):
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return VeiculoListSerializer
+        elif self.action == "retrieve":
+            return VeiculoDetailSerializer
+        return VeiculoSerializer
